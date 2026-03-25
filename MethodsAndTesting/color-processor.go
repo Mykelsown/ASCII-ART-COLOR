e1cb299d79e4f0c923a6004210b0869c6f033aa3
micsamuel <mykelsown@gmail.com>
2026-03-25 16:37:52 +0100

    I have gone back to the very first succesfull painting of the code, after so may trials and errors, i got confused and had to reset and restart

--- MethodsAndTesting/color-processor.go
+++ MethodsAndTesting/color-processor.go
@@ -1,66 +1,124 @@
+// package asciiart
+
+// import (
+// 	"strings"
+// )
+
+// var colorStorage = map[string]string{
+// 	"red":     "\033[31m",
+// 	"green":   "\033[32m",
+// 	"yellow":  "\033[33m",
+// 	"blue":    "\033[34m",
+// 	"magenta": "\033[35m",
+// 	"cyan":    "\033[36m",
+// 	"white":   "\033[37m",
+// 	"black":   "\033[30m",
+
+// 	// Bright variants
+// 	"bright_red":     "\033[91m",
+// 	"bright_green":   "\033[92m",
+// 	"bright_yellow":  "\033[93m",
+// 	"bright_blue":    "\033[94m",
+// 	"bright_magenta": "\033[95m",
+// 	"bright_cyan":    "\033[96m",
+// 	"bright_white":   "\033[97m",
+// }
+
+// func ApplyColor(colorType string, arguments []string) string {
+// 	mainString := arguments[len(arguments)-1] // gets the main string in the argument
+// 	options := arguments[:len(arguments)-1]   // gets the substrings(s) that needs to be colored in the main string from the argument
+
+// 	formatedMainString := FormatPrinter(mainString) //This gets the ascii-art of the main string
+// 	// This is the main string that will store the final output
+// 	var newFormattedMain strings.Builder
+
+// 	// The main logic that will check for each sub-string in the main string and then apply the color to them
+// 	for _, v := range options {
+// 		prefix, suffix, contains := subExistOverOne(mainString, v)
+
+// 		splittedFormattedMainString := strings.Split(formatedMainString, "\n")
+
+// 		var slicedModifiedMainString = make([]string, len(splittedFormattedMainString))
+// 		if contains {
+
+// 			splittedFormattedPrefix := strings.Split(FormatPrinter(prefix), "\n")
+// 			splittedFormattedOption := strings.Split(FormatPrinter(v), "\n")
+// 			splittedFormattedSuffix := strings.Split(FormatPrinter(suffix), "\n")
+
+// 			for i := range splittedFormattedMainString {
+// 				colored := colorStorage[colorType] + splittedFormattedOption[i] + "\033[0m"
+// 				slicedModifiedMainString[i] = splittedFormattedPrefix[i] + colored + splittedFormattedSuffix[i]
+// 			}
+// 		}
+// 		newFormattedMain.WriteString(strings.Join(slicedModifiedMainString, "\n"))
+// 	}
+
+// 	return newFormattedMain.String()
+// }
+
+// func subExistOverOne(main, sub string) (string, string, bool) {
+// 	prefix, suffix, found := strings.Cut(main, sub)
+// 	if !found {
+// 		return "", "", false
+// 	}
+// 	_, _, foundAgain := subExistOverOne(suffix, sub)
+// 	return prefix, suffix, foundAgain
+// }
+
+
 package asciiart
 
 import (
-	"strings"
+    "strings"
 )
 
 var colorStorage = map[string]string{
-	"red":     "\033[31m",
-	"green":   "\033[32m",
-	"yellow":  "\033[33m",
-	"blue":    "\033[34m",
-	"magenta": "\033[35m",
-	"cyan":    "\033[36m",
-	"white":   "\033[37m",
-	"black":   "\033[30m",
+    "red":     "\033[31m",
+    "green":   "\033[32m",
+    "yellow":  "\033[33m",
+    "blue":    "\033[34m",
+    "magenta": "\033[35m",
+    "cyan":    "\033[36m",
+    "white":   "\033[37m",
+    "black":   "\033[30m",
 
-	// Bright variants
-	"bright_red":     "\033[91m",
-	"bright_green":   "\033[92m",
-	"bright_yellow":  "\033[93m",
-	"bright_blue":    "\033[94m",
-	"bright_magenta": "\033[95m",
-	"bright_cyan":    "\033[96m",
-	"bright_white":   "\033[97m",
+    // Bright variants
+    "bright_red":     "\033[91m",
+    "bright_green":   "\033[92m",
+    "bright_yellow":  "\033[93m",
+    "bright_blue":    "\033[94m",
+    "bright_magenta": "\033[95m",
+    "bright_cyan":    "\033[96m",
+    "bright_white":   "\033[97m",
 }
 
 func ApplyColor(colorType string, arguments []string) string {
-	mainString := arguments[len(arguments)-1] // gets the main string in the argument
-	options := arguments[:len(arguments)-1]   // gets the substrings(s) that needs to be colored in the main string from the argument
+    mainString := arguments[len(arguments)-1] // gets the main string in the argument
+    options := arguments[:len(arguments)-1]   // gets the substrings(s) that needs to be colored in the main string from the argument
 
-	formatedMainString := FormatPrinter(mainString) //This gets the ascii-art of the main string
-	// This is the main string that will store the final output
-	var newFormattedMain strings.Builder
+    formatedMainString := FormatPrinter(mainString) //This gets the ascii-art of the main string
+    // This is the main string that will store the final output
+    var newFormattedMain strings.Builder
 
-	// The main logic that will check for each sub-string in the main string and then apply the color to them
-	for _, v := range options {
-		prefix, suffix, contains := subExistOverOne(mainString, v)
+    // The main logic that will check for each sub-string in the main string and then apply the color to them
+    for _, v := range options {
+        formatedOption := FormatPrinter(v) // This stores the sub-string ascii-art
+        // Checks if the substring passed from the terminal really exist in the main string
+        contains := strings.Contains(mainString, v)
 
-		splittedFormattedMainString := strings.Split(formatedMainString, "\n")
+        splittedFormattedMainString := strings.Split(formatedMainString, "\n")
+        splittedFormattedOption := strings.Split(formatedOption, "\n")
 
-		var slicedModifiedMainString = make([]string, len(splittedFormattedMainString))
-		if contains {
+        var slicedModifiedMainString = make([]string, len(splittedFormattedMainString))
+        if contains {
+            for i, asciiChars := range splittedFormattedMainString {
+                splittedColoredFormattedOption := colorStorage[colorType] + splittedFormattedOption[i] + "\033[0m"
+                newAsciiChars := strings.ReplaceAll(asciiChars, splittedFormattedOption[i], splittedColoredFormattedOption)
+                slicedModifiedMainString[i] = newAsciiChars
+            }
+        }
+        newFormattedMain.WriteString(strings.Join(slicedModifiedMainString, "\n"))
+    }
 
-			splittedFormattedPrefix := strings.Split(FormatPrinter(prefix), "\n")
-			splittedFormattedOption := strings.Split(FormatPrinter(v), "\n")
-			splittedFormattedSuffix := strings.Split(FormatPrinter(suffix), "\n")
-
-			for i := range splittedFormattedMainString {
-				colored := colorStorage[colorType] + splittedFormattedOption[i] + "\033[0m"
-				slicedModifiedMainString[i] = splittedFormattedPrefix[i] + colored + splittedFormattedSuffix[i]
-			}
-		}
-		newFormattedMain.WriteString(strings.Join(slicedModifiedMainString, "\n"))
-	}
-
-	return newFormattedMain.String()
-}
-
-func subExistOverOne(main, sub string) (string, string, bool) {
-	prefix, suffix, found := strings.Cut(main, sub)
-	if !found {
-		return "", "", false
-	}
-	_, _, foundAgain := subExistOverOne(suffix, sub)
-	return prefix, suffix, foundAgain
+    return newFormattedMain.String()
 }
