01742310aaf24a12f705906dc4f57032de490224
micsamuel <mykelsown@gmail.com>
2026-03-27 09:12:30 +0100

    Added the condition for inouting the right flag type from the terminal

--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	asciiart "asciiartcolor/MethodsAndTesting"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	var colorValue string
+	flagType := os.Args[1]
 
 	flag.StringVar(&colorValue, "color", "white", "The color of the output")
+
 	flag.Parse()
 	arguments := flag.Args()
-
+	// This checks for wrong flag input the user might pass in through the terminal
+	if flagType != "--color="+colorValue {
+		fmt.Println(`Usage: go run . [OPTION] [STRING]
+EX: go run . --color=<color> <substring to be colored> "something"`)
+		return
+	}
 	fmt.Println(asciiart.ApplyColor(colorValue, arguments))
 }
